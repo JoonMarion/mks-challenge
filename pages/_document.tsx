9753ebb38b6c54bfb66c2ceb45c2864db7cf69f5
JoonMarion <marianojoao52@gmail.com>
2023-02-03 00:26:47 -0300

    finished project

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -36,6 +36,7 @@ export default class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400&family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
                         rel="stylesheet"
                     />
+                    <title>MKS Sistemas</title>
                 </Head>
                 <body>
                     <Main />
